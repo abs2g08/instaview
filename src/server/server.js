e2f658901ba463c7587a8fc62b0524d07bfa6508
abs20g8 <abs2g08@gmail.com>
2015-10-12 23:37:57 -0700

    integrated instagram API

--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,38 @@ import express from 'express';
 import React from 'react';
 import Router from 'react-router';
 import routes from '../app/routes';
+import instagramNode from 'instagram-node'
 
 const app = express();
+const api = instagramNode.instagram();
+
+api.use({
+  client_id: 'YOUR_CLIENT_ID',
+  client_secret: 'YOUR_CLIENT_SECRET'
+});
+
+var redirect_uri = 'http://localhost:3000/handleauth';
+
+exports.authorize_user = function(req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+};
+
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+    if (err) {
+      console.log(err.body);
+      res.send('Didnt work');
+    } else {
+      console.log(`Yay! Access token is ${result.access_token}`);
+      res.send('You made it!!');
+    }
+  });
+};
+
+// This is where you would initially send users to authorize
+app.get('/authorize_user', exports.authorize_user);
+// This is your redirect URI
+app.get('/handleauth', exports.handleauth);
 
 // set up Jade
 app.set('views', './');
