e132c58ee22bca7af174fc5aa309d3b5cbb3c8bb
Adam <abs2g08@gmail.com>
2016-03-31 15:27:32 +0100

    added unit tests for search functionality

--- src/app/tests/home-test.js
+++ src/app/tests/home-test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Router } from 'react-router';
 import Routes from '../Routes';
+import sinon from 'sinon';
+import { HomeStore } from '../stores';
+import { HomeActions } from '../actions';
 import { render, unmountComponentAtNode } from 'react-dom';
 import createHistory from 'history/lib/createMemoryHistory';
 import $ from 'jquery';
@@ -31,5 +34,43 @@ describe('as a User I should be able to navigate to the home page and view its c
   it('should show home feed when user selects login button', (done)=> {
     /* TO-DO: work out how to do this */
     done();
+  });
+});
+
+describe('HomeStore tests', ()=> {
+  let sandbox;
+  let server;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    server = sandbox.useFakeServer();
+  });
+  afterEach(() => {
+    server.restore();
+    sandbox.restore();
+  });
+
+  it('should store medias: [1, 2, 3] from ajax call', (done)=> {
+    HomeActions.getMyFeed();
+
+    setTimeout(() => server.respond([200, {
+      'Content-Type': 'application/json' }, JSON.stringify({ medias: [1, 2, 3] })]), 0);
+
+    HomeStore.listen(function() {
+      const medias = HomeStore.getState().medias.asMutable();
+      expect(medias).toEqual([1,2,3]);
+      done();
+    });
+  });
+  it('should store error message from failed ajax call', (done)=> {
+    HomeActions.getMyFeed();
+
+    setTimeout(() => server.respond([500, {
+      'Content-Type': 'application/json' }, JSON.stringify({ errorMsg: 'bar' })]), 0);
+
+    HomeStore.listen(function() {
+      expect(HomeStore.getState().errorMsg).toEqual('bar');
+      done();
+    });
   });
 });
--- src/app/tests/search-test.js
+++ src/app/tests/search-test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Router } from 'react-router';
 import Routes from '../Routes';
+import sinon from 'sinon';
+import { SearchStore } from '../stores';
+import { SearchActions } from '../actions';
 import { render, unmountComponentAtNode } from 'react-dom';
 import createHistory from 'history/lib/createMemoryHistory';
 import $ from 'jquery';
@@ -28,6 +31,53 @@ describe('as a User I should be able to navigate to the search page and view its
       expect(text).toEqual(text);
       done();
     })
+  });
+});
+
+describe('SearchStore tests', ()=> {
+  let sandbox;
+  let server;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    server = sandbox.useFakeServer();
+  });
+  afterEach(() => {
+    server.restore();
+    sandbox.restore();
+  });
+
+  it('should store users: [1, 2, 3] from ajax call', (done)=> {
+    SearchActions.searchUser('foo');
+
+    setTimeout(() => server.respond([200, {
+      'Content-Type': 'application/json' }, JSON.stringify({ users: [1, 2, 3] })]), 0);
+
+    let count = 0;
+
+    SearchStore.listen(function() {
+      const users = SearchStore.getState().users.asMutable();
+      if(count === 0) {
+        expect(users).toEqual([1,2,3]);
+      }
+
+      if(count === 1) {
+        expect(users).toEqual([]);
+      }
+      count++;
+      done();
+    });
+  });
+  it('should store error message from failed ajax call', (done)=> {
+    SearchActions.searchUser('foo');
+
+    setTimeout(() => server.respond([500, {
+      'Content-Type': 'application/json' }, JSON.stringify({ errorMsg: 'bar' })]), 0);
+
+    SearchStore.listen(function() {
+      expect(SearchStore.getState().errorMsg).toEqual('bar');
+      done();
+    });
   });
 });
 
